00ecee825a493d29704bbd97b563d0d179c490ab
AleksKraken <xxxaleks85xxx@gmail.com>
2024-12-09 23:18:59 +0200

    hw11t2

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,27 @@
+export const searchImage = search => {
+    const searchParams = new URLSearchParams({
+      key: '47510144-3a7cfc25704c8cb682269df00',
+      q: search,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    });
+  
+    const url = `https://pixabay.com/api/?${searchParams}`;
+  
+    const options = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Host: 'http://localhost:5173',
+        Origin: 'https://pixabay.com/api',
+      },
+    };
+  
+    return fetch(url, options).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+  };
